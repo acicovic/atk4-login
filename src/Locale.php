b3d81c1ba0748e9bdb4870a49d65720f1e1ede38
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2020-12-29 21:02:35 +0200

    Add Locale class

---
+++ src/Locale.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atk4\Login;
+
+use Atk4\Core\Exception;
+
+class Locale
+{
+    public function __construct()
+    {
+        throw new Exception('Class Locale is needed only for locating the default translations');
+    }
+
+    /**
+     * Get absolute Path of default translations.
+     */
+    public static function getPath(): string
+    {
+        return dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'locale';
+    }
+}
